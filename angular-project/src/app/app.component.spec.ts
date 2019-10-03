1f6f30da6ba6a4e0d31114627fd9351c73eafc69
katy-holder <gold19920929@gmail.com>
2019-10-03 15:52:26 +0300

    init

--- angular-project/src/app/app.component.spec.ts
+++ angular-project/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'folder'`, () => {
+  it(`should have as title 'angular-project'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('folder');
+    expect(app.title).toEqual('angular-project');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('folder app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('angular-project app is running!');
   });
 });
